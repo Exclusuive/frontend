6d3d54d93bc2348c0af858562600b80f8f6effb7
poqopo <block.poqopo@gmail.com>
2025-03-19 11:19:50 +0900

    Apply Exclusuive Typescript SDK

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,24 @@
-export default function Dashboard() {
+import { useImageOverlay, ImageOverlay } from "exclusuive-typescript-sdk";
+
+const App = () => {
+  const { images } = useImageOverlay({
+    imageUrls: [
+      "https://myyonseinft.s3.us-east-1.amazonaws.com/MAJOR/test/background.PNG",
+      "https://myyonseinft.s3.us-east-1.amazonaws.com/MAJOR/test/character.png",
+      "https://myyonseinft.s3.us-east-1.amazonaws.com/MAJOR/test/clothes.PNG",
+    ],
+  });
+  console.log(images);
   return (
-    <div>
-      <h1>Hello It's Dashboard</h1>
-    </div>
+    <ImageOverlay
+      images={images}
+      sizes={[
+        { width: 300, height: 300 },
+        { width: 300, height: 300 },
+        { width: 300, height: 300 },
+      ]}
+    />
   );
-}
+};
+
+export default App;
