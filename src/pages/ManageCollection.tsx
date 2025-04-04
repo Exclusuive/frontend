9ca3f9feae6fa6701bf1d66605a4b197aaddc046
Modi04 <ydjeong03@gmail.com>
2025-04-04 16:47:45 +0000

    ui깨지는거 수정중

--- src/pages/ManageCollection.tsx
+++ src/pages/ManageCollection.tsx
@@ -25,18 +25,18 @@ export default function Dashboard() {
   return (
     <div className="pt-5 pr-18 pl-18">
       <div className="pb-2 text-5xl">Dashboard</div>
-      <div className="flex w-full items-center justify-between">
+      <div className="flex flex-wrap w-full items-center justify-between">
         <div className="text-2xl">Collections</div>
         <button
           onClick={() => navigate("/createcollection")}
-          className="cursor-pointer rounded-lg bg-blue-500 px-4 py-2 text-white transition hover:bg-blue-600"
+          className="min-w-[200px] cursor-pointer rounded-lg bg-blue-500 px-4 py-2 text-white transition hover:bg-blue-600"
         >
           Create a new collection
         </button>
       </div>
 
       <div className="mx-auto w-3/4">
-        <div className="grid w-full grid-cols-2 justify-center gap-x-10 py-10">
+        <div className="grid w-[680px] place-items-center grid-cols-2 gap-y-10 py-10">
           {data.map((item) => (
             <Link to={`/collection/${item.collectionId}/${item.capId}`}>
               <CollectionCard item={item}></CollectionCard>
