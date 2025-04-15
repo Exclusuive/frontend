05821d8d1ed69c5c126ab23ee1bda628d0adaabd
poqopo <block.poqopo@gmail.com>
2025-04-15 11:18:32 +0900

    Modify Intro

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,7 @@
 export default function Dashboard() {
   return (
     <div>
-      <h1>Hello It's Dashboard</h1>
+      <h1>Welcome to the Exclusuive. Please Login and Look Around</h1>
     </div>
   );
 }
