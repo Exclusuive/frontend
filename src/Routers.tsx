7a4a958f72097c0ca6cf7af374e089a9b14812fd
guboc11 <guboc11@gmail.com>
2025-04-29 16:37:18 +0900

    Set page structure

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -2,12 +2,26 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import LandingPage from "./pages/LandingPage";
 
+import Collection from "./pages/admin/Collection/Page";
+import MembershipPolicy from "./pages/admin/MembershipPolicy/Page";
+
+import MyMembership from "./pages/members/MyMembership/Page";
+import MembershipStore from "./pages/members/MembershipStore/Page";
+
 export default function Routers() {
   return (
     <Router>
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<LandingPage />} />
+          <Route path="admin">
+            <Route path="collection" element={<Collection />} />
+            <Route path="membershippolicy" element={<MembershipPolicy />} />
+          </Route>
+          <Route path="members">
+            <Route path="mymembership" element={<MyMembership />} />
+            <Route path="membershipstore" element={<MembershipStore />} />
+          </Route>
         </Route>
       </Routes>
     </Router>
---
+++ src/pages/admin/Collection/Page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Hello It's Collection</h1>
+    </div>
+  );
+}
