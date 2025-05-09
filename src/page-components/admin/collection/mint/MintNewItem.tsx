d45f321f953435faf9982e049fcaaa5f481dea0c
guboc11 <guboc11@gmail.com>
2025-05-10 01:27:40 +0900

    Update MintNewItem UI & states

--- src/page-components/admin/collection/mint/MintNewItem.tsx
+++ src/page-components/admin/collection/mint/MintNewItem.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/components/ui/card";
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogFooter,
   DialogHeader,
@@ -24,11 +25,49 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { CollectionContext } from "@/context/CollectionContext";
+import { CollectionData } from "@/types/collection";
+import { useContext, useEffect, useState } from "react";
 import { ImagePlus } from "lucide-react";
 
 export default function MintNewItem() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [currentCollection, setCurrentCollection] = useState<CollectionData>();
+  const [layer, setLayer] = useState<string>("");
+  const [itemType, setItemType] = useState<string>("");
+  const [isNewItem, setIsNewItem] = useState<boolean>(false);
+  const [property, setProperty] = useState<string>("");
+  const [propertyValue, setPropertyValue] = useState<number>(0);
+  const [addedProperties, setAddedProperties] = useState<Record<string, number>>();
+  const [recipient, setRecipient] = useState<string>("");
+  const [imageFile, setImageFile] = useState<File>();
+
+  const {
+    collection: { collections, index, refetch },
+  } = useContext(CollectionContext);
+
+  useEffect(() => {
+    if (collections && index !== -1) {
+      setCurrentCollection(collections[index]);
+    }
+  }, [collections, index]);
+
+  useEffect(() => {
+    resetForm();
+  }, [isOpen]);
+
+  const resetForm = () => {
+    setLayer("");
+    setItemType("");
+    setProperty("");
+    setPropertyValue(0);
+    setAddedProperties(undefined);
+    setRecipient("");
+    setImageFile(undefined);
+  };
+
   return (
-    <Dialog>
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogTrigger>
         <Card className="cursor-pointer transition-all hover:shadow-lg">
           <CardHeader>
@@ -45,49 +84,75 @@ export default function MintNewItem() {
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>
-            {/* {mode === "new" ? "Create and Mint New Item" : "Mint Existing Item"} */}
-            {"Create and Mint New Item"}
-          </DialogTitle>
+          <DialogTitle>{"Create and Mint New Item"}</DialogTitle>
         </DialogHeader>
 
-        <div className="grid gap-4 py-4">
+        <div className="scrollbar-hide flex h-[500px] flex-col gap-4 overflow-scroll py-4">
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="layer" className="text-right">
               Layer
             </Label>
             <Select
-              // value={state.layer}
-              value={"state.layer"}
-              // onValueChange={(value) => setState((prev) => ({ ...prev, layer: value }))}
-              onValueChange={(value) => {}}
+              value={layer}
+              onValueChange={(value) => {
+                setLayer(value);
+              }}
             >
               <SelectTrigger className="col-span-3">
-                <SelectValue>Select a layer</SelectValue>
+                <SelectValue placeholder="Select an layer">{layer}</SelectValue>
               </SelectTrigger>
               <SelectContent>
-                {/* {layerTypes.map((layerType) => (
-                  <SelectItem key={layerType} value={layerType}>
-                    {layerType}
-                  </SelectItem>
-                ))} */}
+                {currentCollection &&
+                  currentCollection.objectData.content.fields.layer_types.fields.contents.map(
+                    (layer) => (
+                      <SelectItem key={layer.fields.type} value={layer.fields.type}>
+                        {layer.fields.type}
+                      </SelectItem>
+                    )
+                  )}
               </SelectContent>
             </Select>
           </div>
 
-          {/* {mode === "new" ? ( */}
-          {"new" === "new" ? (
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="new-existed" className="text-right">
+              New / Existed
+            </Label>
+            <div className="col-span-3 flex gap-2 space-y-2">
+              <Button
+                className={`${isNewItem ? "cursor-pointer" : ""}`}
+                variant={isNewItem ? "secondary" : "default"}
+                onClick={() => {
+                  setIsNewItem(false);
+                }}
+              >
+                Existed Item
+              </Button>
+              <Button
+                className={`${!isNewItem ? "cursor-pointer" : ""}`}
+                variant={!isNewItem ? "secondary" : "default"}
+                onClick={() => {
+                  setIsNewItem(true);
+                  setItemType("");
+                }}
+              >
+                New Item
+              </Button>
+            </div>
+          </div>
+
+          {isNewItem ? (
             <>
               <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="itemName" className="text-right">
-                  Item Name
+                <Label htmlFor="itemType" className="text-right">
+                  Item Type
                 </Label>
                 <Input
-                  id="itemName"
-                  // value={state.itemName}
-                  value={"state.itemName"}
-                  // onChange={(e) => setState((prev) => ({ ...prev, itemName: e.target.value }))}
-                  onChange={(e) => {}}
+                  id="itemType"
+                  value={itemType}
+                  onChange={(e) => {
+                    setItemType(e.target.value);
+                  }}
                   className="col-span-3"
                   placeholder="Enter item name"
                 />
@@ -101,10 +166,10 @@ export default function MintNewItem() {
                     className="flex h-32 w-full cursor-pointer items-center justify-center rounded-md border-2 border-dashed border-gray-300 hover:border-gray-400"
                     onClick={() => document.getElementById("itemImage")?.click()}
                   >
-                    {/* {state.itemImage ? (
+                    {imageFile ? (
                       <div className="relative h-full w-full">
                         <img
-                          src={URL.createObjectURL(state.itemImage)}
+                          src={URL.createObjectURL(imageFile)}
                           alt="Selected item"
                           className="h-full w-full object-contain"
                         />
@@ -114,13 +179,18 @@ export default function MintNewItem() {
                         <ImagePlus className="mx-auto h-12 w-12 text-gray-400" aria-hidden="true" />
                         <div className="mt-2">Click to upload image</div>
                       </div>
-                    )} */}
+                    )}
                   </div>
-                  <input
+                  <Input
                     id="itemImage"
                     type="file"
                     accept="image/*"
-                    // onChange={handleFileChange}
+                    onChange={(e) => {
+                      const file = e.target.files?.[0] || null;
+                      if (file) {
+                        setImageFile(file);
+                      }
+                    }}
                     className="hidden"
                   />
                 </div>
@@ -129,32 +199,21 @@ export default function MintNewItem() {
           ) : (
             <div className="grid grid-cols-4 items-center gap-4">
               <Label htmlFor="item" className="text-right">
-                Item
+                Item Type
               </Label>
-              <Select
-              // value={state.selectedItemId}
-              // onValueChange={(value) => {
-              //   setState((prev) => ({ ...prev, selectedItemId: value }));
-              //   const selectedItem = filteredItems.find((item) => item.name === value);
-              //   if (selectedItem) {
-              //     setState((prev) => ({
-              //       ...prev,
-              //       layer: selectedItem.layer,
-              //       itemName: selectedItem.name,
-              //       selectedItemImageUrl: selectedItem.img_url,
-              //     }));
-              //   }
-              // }}
-              >
+              <Select value={itemType} onValueChange={(value) => setItemType(value)}>
                 <SelectTrigger className="col-span-3">
                   <SelectValue placeholder="Select an item" />
                 </SelectTrigger>
                 <SelectContent>
-                  {/* {filteredItems.map((item) => (
-                    <SelectItem key={item.name} value={item.name}>
-                      {item.name}
-                    </SelectItem>
-                  ))} */}
+                  {currentCollection &&
+                    currentCollection.objectData.content.fields.item_types.fields.contents
+                      .filter((item) => item.fields.type.fields.type === layer)
+                      .map((item) => (
+                        <SelectItem key={item.fields.item_type} value={item.fields.item_type}>
+                          {item.fields.item_type}
+                        </SelectItem>
+                      ))}
                 </SelectContent>
               </Select>
             </div>
@@ -168,21 +227,19 @@ export default function MintNewItem() {
                 <Label htmlFor="propertyType" className="text-right">
                   Property Type
                 </Label>
-                <Select
-                // value={state.selectedPropertyType}
-                // onValueChange={(value) =>
-                //   setState((prev) => ({ ...prev, selectedPropertyType: value }))
-                // }
-                >
+                <Select value={property} onValueChange={(value) => setProperty(value)}>
                   <SelectTrigger className="col-span-3">
-                    <SelectValue placeholder="Select a property type" />
+                    <SelectValue placeholder="Select a property type">{property}</SelectValue>
                   </SelectTrigger>
                   <SelectContent>
-                    {/* {propertyTypes.map((propertyType) => (
-                      <SelectItem key={propertyType} value={propertyType}>
-                        {propertyType}
-                      </SelectItem>
-                    ))} */}
+                    {currentCollection &&
+                      currentCollection.objectData.content.fields.property_types.fields.contents.map(
+                        (property) => (
+                          <SelectItem key={property.fields.type} value={property.fields.type}>
+                            {property.fields.type}
+                          </SelectItem>
+                        )
+                      )}
                   </SelectContent>
                 </Select>
               </div>
@@ -193,18 +250,23 @@ export default function MintNewItem() {
                 </Label>
                 <div className="col-span-3 flex gap-2">
                   <Input
-                    id="propertyValue"
-                    // value={state.propertyValue}
-                    // onChange={(e) =>
-                    //   setState((prev) => ({ ...prev, propertyValue: e.target.value }))
-                    // }
+                    id={property + propertyValue}
+                    value={propertyValue}
+                    onChange={(e) => setPropertyValue(Number(e.target.value))}
                     className="flex-1"
-                    placeholder="Enter property value"
+                    placeholder="Input a property value"
                   />
                   <Button
+                    className="cursor-pointer"
                     type="button"
-                    // onClick={handleAddProperty}
-                    // disabled={!state.selectedPropertyType || !state.propertyValue}
+                    onClick={() => {
+                      if (!property) return;
+
+                      setAddedProperties((prev) => ({ ...prev, [property]: propertyValue }));
+                      setProperty("");
+                      setPropertyValue(0);
+                    }}
+                    disabled={!propertyValue}
                   >
                     Add
                   </Button>
@@ -212,31 +274,33 @@ export default function MintNewItem() {
               </div>
 
               {/* Display added properties */}
-              {/* {state.properties.length > 0 && ( */}
-              {true && (
-                <div className="col-span-4 space-y-2">
-                  <Label className="block text-right">Added Properties</Label>
-                  <div className="space-y-2">
-                    {/* {state.properties.map((property, index) => (
+              <div className="col-span-4 space-y-2">
+                <Label className="block text-right">Added Properties</Label>
+                <div className="space-y-2">
+                  {addedProperties &&
+                    Object.keys(addedProperties).map((property, i) => (
                       <div
-                        key={index}
+                        key={i}
                         className="flex items-center justify-between rounded-md border p-2"
                       >
                         <div>
-                          <span className="font-medium">{property.type}:</span> {property.value}
+                          <span className="font-medium">{property}:</span>{" "}
+                          {addedProperties[property]}
                         </div>
                         <Button
+                          className="cursor-pointer"
                           variant="ghost"
                           size="sm"
-                          onClick={() => handleRemoveProperty(index)}
+                          onClick={() => {
+                            delete addedProperties[property];
+                          }}
                         >
                           Remove
                         </Button>
                       </div>
-                    ))} */}
-                  </div>
+                    ))}
                 </div>
-              )}
+              </div>
             </div>
           )}
 
@@ -246,25 +310,34 @@ export default function MintNewItem() {
             </Label>
             <Input
               id="recipient"
-              // value={state.recipient}
-              // onChange={(e) => setState((prev) => ({ ...prev, recipient: e.target.value }))}
+              value={recipient}
+              onChange={(e) => setRecipient(e.target.value)}
               className="col-span-3"
               placeholder="Enter recipient address"
             />
           </div>
         </div>
         <DialogFooter>
-          <Button
-            variant="outline"
-            onClick={() => {
-              // onClose();
-              // setState(initialState);
-            }}
-          >
-            Cancel
-          </Button>
-          {/* <Button onClick={handleMint} disabled={state.isLoading}> */}
-          <Button>{/* {state.isLoading ? "Minting..." : "Mint"} */}</Button>
+          <DialogClose>
+            <Button
+              variant="outline"
+              onClick={() => {
+                resetForm();
+              }}
+            >
+              Cancel
+            </Button>
+          </DialogClose>
+          <DialogClose>
+            <Button
+              className="cursor-pointer"
+              onClick={() => {
+                resetForm();
+              }}
+            >
+              Mint
+            </Button>
+          </DialogClose>
         </DialogFooter>
       </DialogContent>
     </Dialog>
