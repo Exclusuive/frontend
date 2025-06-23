ddafc5dced777f947d668ccfe5713c8540c6697e
poqopo <block.poqopo@gmail.com>
2025-06-23 14:10:30 +0900

    Fix.Provider Duplicate

--- src/components/provider/provider.tsx
+++ src/components/provider/provider.tsx
@@ -2,10 +2,19 @@ import { queryConfig } from "@/constants/react-query";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
+import { createNetworkConfig, SuiClientProvider, WalletProvider } from "@mysten/dapp-kit";
+import { getFullnodeUrl } from "@mysten/sui/client";
 
 interface AppProviderProps {
   children: React.ReactNode;
 }
+
+// Config options for the networks you want to connect to
+const { networkConfig } = createNetworkConfig({
+  testnet: { url: getFullnodeUrl("testnet") },
+  localnet: { url: getFullnodeUrl("localnet") },
+  mainnet: { url: getFullnodeUrl("mainnet") },
+});
 
 export const AppProvider = ({ children }: AppProviderProps) => {
   const [queryClient] = useState(
@@ -17,8 +26,12 @@ export const AppProvider = ({ children }: AppProviderProps) => {
 
   return (
     <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
+      <SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
+        <WalletProvider>
+          {children}
+          <ReactQueryDevtools initialIsOpen={false} />
+        </WalletProvider>
+      </SuiClientProvider>
     </QueryClientProvider>
   );
 };
