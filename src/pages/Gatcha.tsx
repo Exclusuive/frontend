308552d421556510594eeac7912352e41fdef6c0
poqopo <block.poqopo@gmail.com>
2025-08-23 12:00:10 +0900

    Join Gatcha

--- src/pages/Gatcha.tsx
+++ src/pages/Gatcha.tsx
@@ -94,10 +94,16 @@ const Gatcha = () => {
               <Button
                 className="w-full transform rounded-2xl bg-gradient-to-r from-blue-600 to-cyan-600 px-6 py-4 text-lg font-semibold text-white shadow-lg transition-all duration-200 hover:scale-[1.02] hover:from-blue-700 hover:to-cyan-700"
                 onClick={() => {
-                  joinGatcha({
-                    membership_id: membership_id ?? "",
-                    isNew: joined_Gatcha !== "true",
-                  });
+                  if (account) {
+                    if (userHasMembership) {
+                      joinGatcha({
+                        membership_id: membership_id ?? "",
+                        isNew: joined_Gatcha !== "true",
+                      });
+                    } else {
+                      console.log("no membership");
+                    }
+                  }
                 }}
               >
                 Join Gatcha
