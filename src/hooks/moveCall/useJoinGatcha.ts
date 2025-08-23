e250aa434569e0a4ce846870f59ba7fc1305dd26
poqopo <block.poqopo@gmail.com>
2025-08-23 14:05:23 +0900

    Remove serialize

--- src/hooks/moveCall/useJoinGatcha.ts
+++ src/hooks/moveCall/useJoinGatcha.ts
@@ -70,7 +70,7 @@ export function useJoinGatcha() {
 
     signAndExecuteTransaction(
       {
-        transaction: tx.serialize(),
+        transaction: tx,
       },
       {
         onSuccess: () => {
